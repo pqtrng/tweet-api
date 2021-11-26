48f62b6e2c44c5ce6bc11af64f9e71337a5d626b
Pham, Trung <72325861+pqtrng@users.noreply.github.com>
2021-11-26 15:19:52 +0700

    Add config

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from . import models
+from .database import engine
+
+# Connect to database
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello, World."}
